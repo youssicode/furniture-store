8278a68e5d296a6f054cfcf36a214f3a4eadab63
Youssef <78873191+youssicode@users.noreply.github.com>
2021-11-24 17:09:34 +0000

    Add files via upload

--- js/script.js
+++ js/script.js
@@ -1,20 +1,22 @@
-/* Carousel with JavaScript */ 
-
+//* Variables 
 let slideNbr = 1;
+var cntrl = true;
+let updatedTotPrice = 560;
+displayNewPrice(updatedTotPrice);
+//* Carousel with JavaScript */ 
 displaySlide (1,2,3)
 function slideRight () {
-  
   if (slideNbr == 1) {
     displaySlide (2,1,3);
     slideNbr++;
-    } else if (slideNbr == 2) {
-      displaySlide (3,1,2);
-      slideNbr++;
-    } else { // Control if is the last slide.
+  } else if (slideNbr == 2) {
+    displaySlide (3,1,2);
+    slideNbr++;
+  } else { // Control if is the last slide.
       displaySlide (1,3,2);
       slideNbr = 1;
-  }
-}  
+    }
+  }  
 function slideLeft () {
   if (slideNbr == 1) {
     displaySlide (3,1,2);
@@ -33,11 +35,21 @@ function displaySlide (active,dsbl1,dsbl2) {
   document.getElementById(`slideId${active}`).style.display= "grid";
 }
 
-// Open and Close Popup's windows 
+//! Open and Close Popup's windows 
 closePopup() //we call it on page loading to hide all popup window's elements, we can initialise this in css file if we want
 function displayNav() {
   displayRightNav();
   document.getElementById("navId").style.display= "initial";
+}
+
+function displaySrchForm() {
+  if (window.cntrl == true) {
+    document.getElementById("srch-form").classList.add("srchFormWide");
+    cntrl = false;
+  } else {
+    document.getElementById("srch-form").classList.remove("srchFormWide");
+    cntrl = true;
+  }
 }
 function displayCart() {
   displayRightNav();
@@ -48,8 +60,8 @@ function displayUser() {
   document.getElementById("userId").style.display= "initial";
 }
 function closePopup() {
-  document.getElementById("popupId").style.width= "0";
   document.getElementById("rightNav").style.width= "0";
+  document.getElementById("popupId").style.width= "0";
   document.getElementById("navId").style.display= "none";
   document.getElementById("cartId").style.display= "none";
   document.getElementById("userId").style.display= "none";
@@ -62,11 +74,10 @@ function displayRightNav() {
 }
 
 // Delete Items From Cart 
-let updatedTotPrice = 560;
+
 function displayNewPrice(updatedTotPrice) {
   document.getElementById("totPrice").innerHTML = "$" + updatedTotPrice + ".00";
 }
-displayNewPrice(updatedTotPrice);
 function deleteFromCart(elementClickedOn) {
   let itemNbr = elementClickedOn.slice(-1);
 document.getElementById(`cartItem${itemNbr}`).style.display= "none";
